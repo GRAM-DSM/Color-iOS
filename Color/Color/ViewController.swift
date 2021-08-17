49c683331af31fac8ca7c4e8b885cb798241ba45
jangseoyoung <jangseoyoung0402@gmail.com>
2021-08-17 13:09:00 +0900

    [CREATE] First Setting

---
+++ Color/Color/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color
+//
+//  Created by 장서영 on 2021/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
