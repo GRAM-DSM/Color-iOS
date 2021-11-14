5cf709ff6998c04649d303553290bacaf5222a8b
jangseoyoung <jangseoyoung0402@gmail.com>
2021-11-14 23:49:43 +0900

    [UPDATE] UI

---
+++ Color/Color/VC/Introduction/AngryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AngryViewController.swift
+//  Color
+//
+//  Created by 장서영 on 2021/10/26.
+//
+
+import UIKit
+
+class AngryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Color/Color/VC/Introduction/ShameViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ShameViewController.swift
+//  Color
+//
+//  Created by 장서영 on 2021/10/26.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class ShameViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
+    
+    @IBOutlet weak private var backButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        backButton.rx.tap
+            .bind(onNext: {[unowned self] in
+                navigationController?.popViewController(animated: true)
+            }).disposed(by: disposeBag)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
